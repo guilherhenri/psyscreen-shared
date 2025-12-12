72d4f60b456c5ddfda5b039ddf3ff45610c87034
Guilherme Henrique <guilherhenri12@gmail.com>
2025-12-12 14:11:24 -0300

    fix(contracts): fix jwks topic name

--- packages/contracts/src/auth/topics.ts
+++ packages/contracts/src/auth/topics.ts
@@ -2,5 +2,5 @@ export enum AuthTopics {
   REGISTER_USER = 'auth.command.register',
   LOGIN_USER = 'auth.command.login',
   REFRESH_TOKEN = 'auth.command.refresh',
-  JWKS_KEYS = 'auth.command.jwks',
+  JWKS = 'auth.command.jwks',
 }
